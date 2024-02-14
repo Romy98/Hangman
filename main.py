ab1305b7a430768ac5911fea0a910e5bd8b1b20d
Romy-Mukendi <70291492+Romy-Mukendi@users.noreply.github.com>
2024-02-14 10:15:41 -0700

    took another approach and one function (hangman) for most one the work

--- main.py
+++ main.py
@@ -1,40 +1,59 @@
+
 import random
 
-words = ['computer', 'python', 'entrepreneur', 'one', 'piece']
-correct_letters = ['']
-wrong_letters = ['']
-correct_answers = 0
-is_over = False
+def choose_word():
+    words = ["hangman", "python", "programming", "developer", "coding", "challenge"]
+    return random.choice(words)
 
-def word_to_guess(words):
-    word = random.choice(words)
-    return word
-
-def choose_letter():
-    tries = 6
-    word = list(word_to_guess(words))
-    masked = []
+def display_word(word, guessed_letters):
+    display = ""
     for letter in word:
-        masked.append('-')
-    print(f'This is the word you need to guess: {masked}\nIt has {len(masked)} letters')
-    while (tries > 0):
-        count = 0
-        letter = input('Enter a letter: ')
-        for x in word:
-            if letter in word:
-                masked[word.index(letter)] = letter
-                correct_letters.append('letter')
-                print(masked)
-                count += 1
-                break
-            elif letter not in word:
-                print('Wrong pick! Try again.')
-                wrong_letters.append('letter')
-                if tries == 1:
-                    print("Game Over!")
-                count += 1
-                tries -= 1
-                break
+        if letter in guessed_letters:
+            display += letter
+        else:
+            display += "_"
+    return display
+
+def hangman():
+    max_attempts = 6
+    lives = max_attempts
+    guessed_letters = []
+    word_to_guess = choose_word()
+
+    print("Welcome to Hangman!")
+    print(display_word(word_to_guess, guessed_letters))
+    print(f"You have {lives} lives.")
+
+    while lives > 0:
+        guess = input("Guess a letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid input. Please enter a single letter.")
+            continue
+
+        if guess in guessed_letters:
+            print("You already guessed that letter. Try again.")
+            continue
+
+        guessed_letters.append(guess)
+
+        if guess not in word_to_guess:
+            lives -= 1
+            print(f"Wrong guess! Lives left: {lives}")
+        else:
+            print("Good guess!")
+
+        current_display = display_word(word_to_guess, guessed_letters)
+        print(current_display)
+
+        if "_" not in current_display:
+            print(f"Congratulations! You guessed the word '{word_to_guess}'.")
+            break
+
+    if "_" in current_display:
+        print(f"Sorry, you ran out of lives. The word was '{word_to_guess}'.")
 
 
-choose_letter()
+
+if __name__ == "__main__":
+    hangman()
