b17bfd31590fcbab5853e99a8a420cd7b87ae467
Romy-Mukendi <70291492+Romy-Mukendi@users.noreply.github.com>
2024-02-13 20:54:50 -0700

    Change in the choose_letter function

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import random
 
-words = ['computer', 'python', 'entrepreneur', 'One', 'Piece']
+words = ['computer', 'python', 'entrepreneur', 'one', 'piece']
 correct_letters = ['']
 wrong_letters = ['']
 correct_answers = 0
-tries = 6
 is_over = False
 
 def word_to_guess(words):
@@ -12,11 +11,30 @@ def word_to_guess(words):
     return word
 
 def choose_letter():
+    tries = 6
     word = list(word_to_guess(words))
     masked = []
     for letter in word:
         masked.append('-')
     print(f'This is the word you need to guess: {masked}\nIt has {len(masked)} letters')
-    letter = input('Enter a letter: ')
+    while (tries > 0):
+        count = 0
+        letter = input('Enter a letter: ')
+        for x in word:
+            if letter in word:
+                masked[word.index(letter)] = letter
+                correct_letters.append('letter')
+                print(masked)
+                count += 1
+                break
+            elif letter not in word:
+                print('Wrong pick! Try again.')
+                wrong_letters.append('letter')
+                if tries == 1:
+                    print("Game Over!")
+                count += 1
+                tries -= 1
+                break
+
 
 choose_letter()
